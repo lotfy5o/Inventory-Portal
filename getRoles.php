8cb91d58cdb071da580b5f4c4312b72bbbf744c5
lotfy5o <lotfymoh1235o@gmail.com>
2023-09-20 14:44:09 +0200

    write the edit button for userCRUD

--- getRoles.php
+++ getRoles.php
@@ -6,16 +6,18 @@ function getRoles(){
     if ($con){
         $query = "CALL st_getRoles();";
         $result = mysqli_query($con, $query);
+        while(mysqli_next_result($con)){;}
+
         if (mysqli_num_rows($result) > 0) {
-            echo "<select class='form-select' name='roleDD'";
+            echo "<select class='form-select col-2' name='roleDD'";
             while($row = mysqli_fetch_row($result)){
                 // the id ($row[0]) || name ($row[1])
                 echo "<option value='$row[0]'>$row[1]</option>";
             }
             echo "</select>";
-            mysqli_close($con);
+            // mysqli_close($con);
         }
-    
+        
     }
     else {
         echo "no";
--- usersCRUD.php
+++ usersCRUD.php
@@ -54,20 +54,23 @@
         <?php 
         session_start();
         if (isset($_POST['editBtn'])){
+            // $_POST['idRB'] == when you chech the radio the $_post['idRB] is set
           if (isset($_POST['idRB'])){
             $_SESSION['userID'] = $_POST['idRB'];
+            retrieve4Edit();
           }
           else {
             echo "<p class='alert alert-danger'>Please Select any User First.</p>";
+            retrieve();
           }
         }
 
-        ?>
         
-        <?php  
+        
+          
         include "connection.php";
         
-        retrieve();
+        // retrieve();
         ?>
     </div>
 </body>
@@ -80,12 +83,13 @@ function retrieve(){
     if ($con){
         $query = "CALL st_getUsers();";
         $result = mysqli_query($con, $query);
+        while(mysqli_next_result($con)){;}
 
         if (mysqli_num_rows($result) > 0){
 
 
 
-            echo "<form action='' method='post'>";
+            echo "<form action='' method='post'class='col-10 offset-1'>";
             echo "<table class='table table-active'>";
             echo "<thead class='table table-active my-3'>";
                 echo "<th>ID</th>" . 
@@ -127,7 +131,95 @@ function retrieve(){
     }
 }
 
+function retrieve4Edit(){
+    global $con;
+    if ($con){
+        while(mysqli_next_result($con)){;}
+        $query = "CALL st_getUsers();";
+        $result = mysqli_query($con, $query);
+        while(mysqli_next_result($con)){;}
+
+
+
+        if (mysqli_num_rows($result) > 0){
+
+            echo "<form action='' method='post' class='col-10 offset-1'>";
+            echo "<table class='table table-active col-6'>";
+            echo "<thead class='table table-active my-3'>";
+                echo "<th>ID</th>" . 
+                "<th>Name</th>" . 
+                "<th>Email</th>" . 
+                "<th>Phone</th>" . 
+                "<th>Role</th>" . 
+                "<th>Username</th>" .
+                "<th>Password</th>" .
+                // "<th>RoleID</th>" . 
+                "<th class='col-3'>Actions</th>" ;
+            echo "</thead>";
+                 while ($row = mysqli_fetch_row($result)){
+                    // if the id in idRB matches the id in the row of the table
+                    if ($_SESSION['userID'] == $row[0]){
+                        
+                        
+                        echo "<tr>";
+                            echo    "<td><input type='radio'   value='$row[0]' name='idRB' class='custom-radio' checked/></td>" . 
+                                    "<td><input type='textbox' value='$row[1]' name='nameTxt' class='form-control'/></td>" . 
+                                    "<td><input type='textbox' value='$row[2]' name='emailTxt' class='form-control'/></td>" . 
+                                    "<td><input type='textbox' value='$row[3]' name='phoneTxt' class='form-control'/></td><td>" ;
+                                    echo getRoles();
+                                    echo "</td>" . 
+
+                                    "<td><input type='textbox' value='$row[4]' name='usernameTxt' class='form-control'/></td>" . 
+                                    "<td><input type='textbox' value='$row[5]' name='passTxt' class='form-control'/></td>" . 
+
+                                    "<td>" . 
+                                    "<input type='submit' value='EDIT' name='editBtn' class='btn btn-info mx-1'/>".
+                                    "<input type='submit' value='UPDATE' name='updateBtn' class='btn btn-info mx-1'/>".
+                                    "<input type='submit' value='DELETE' name='deleteBtn' class='btn btn-info mx-1'/>".
+                                    "</td>";
+
+                        echo "</tr>";
+                    }
+                    else {
+                        echo "<tr>";
+                            echo "<td><input type='radio' name='idRB' value='$row[0]'/></td>" . 
+                                "<td>$row[1]</td>" . 
+                                "<td>$row[2]</td>" . 
+                                "<td>$row[3]</td>" .
+                                "<td>$row[7]</td>" . 
+                                "<td>$row[4]</td>" . 
+                                "<td>$row[5]</td>" . 
+                                
+                                '<td>' .
+                                '<input type="submit" name="editBtn" value="Edit" class="btn btn-info mx-1" />' . 
+                                '<input type="submit" name="updateBtn" value="Update" class="btn btn-info mx-1" />' . 
+                                '<input type="submit" name="DeleteBtn" value="Delete" class="btn btn-info mx-1" />' .
+                                '</td>';
+                              
+                                
+                    }
+                    echo "</tr>";
+                                      
+                }
+            echo "</table>";        
+            echo "</form>";
+            
+        }
+        else {
+            echo "No Record Are Available";
+        }
+    }
+    else {
+        echo "No";
+    }
+
+}
+if (isset($_POST['viewBtn'])){
+    retrieve();
+}
 ?>
+
+
 
 
 
